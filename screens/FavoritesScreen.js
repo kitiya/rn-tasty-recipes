0ab0f07e8832afec02a6e6b62de3960baa8be6f6
Kitiya <kate.kitiya@gmail.com>
2020-06-26 17:31:30 -0400

    153. Rendering a fallback text (for empty favorite)

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { View, StyleSheet } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import { useSelector } from "react-redux";
 
 import HeaderButton from "../components/HeaderButton";
+import DefaultText from "../components/DefaultText";
 import RecipeList from "../components/RecipeList";
 
 const FavoritesScreen = (props) => {
   const favRecipe = useSelector((state) => state.recipes.favoriteRecipes);
+
+  if (favRecipe.length === 0 || !favRecipe) {
+    return (
+      <View style={styles.emptyFav}>
+        <DefaultText>No favorite recipes found. Start adding some!</DefaultText>
+      </View>
+    );
+  }
 
   return (
     <RecipeList recipeListData={favRecipe} navigation={props.navigation} />
@@ -32,4 +42,11 @@ FavoritesScreen.navigationOptions = (navData) => {
   };
 };
 
+const styles = StyleSheet.create({
+  emptyFav: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 export default FavoritesScreen;
